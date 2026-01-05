ddc1edff75c38e219d1c85140a63dc4e5fe45ab3
Kingsley Maduabuchi <deblessedking001@gmail.com>
2026-01-05 12:00:06 +0100

    feat(day1): add variables and types exercises with updated learning hacks

---
+++ JS-practice/day1.js
@@ -0,0 +1,11 @@
+//The Online Products variabe
+const name = "Wireless mouse"; //String
+console.log(typeof name);
+const price = 29.99; //number
+console.log(typeof price);
+const instock = true;    //boolean
+console.log(typeof instock);
+const discount = null;   //null
+console.log(typeof discount);
+const deletedTimestamp = undefined; //undefined
+console.log(typeof deletedTimestamp);
